361347b28bb4e097b29bbe0f77f565220db4f30f
Chris Northwood <chris.northwood@hotmail.com>
2017-11-21 20:23:36 +0000

    function now searches db and finds famous people by first or last name and logs results

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,5 @@
 const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const settings = require("./settings");
 
 const client = new pg.Client({
   user     : settings.user,
@@ -11,14 +11,10 @@ const client = new pg.Client({
 });
 
 client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log(result.rows[0].number); //output: 1
+  client.query("SELECT * FROM famous_people WHERE last_name =$1 OR first_name = $1", [process.argv[2]], (err, result) => {
+    console.log('Searching...')
+    console.log(result.rows.length +` person by the name '`+ process.argv[2] + `'`);
+    console.log(result.rows[0].first_name, result.rows[0].last_name + ", born " + result.rows[0].birthdate.toLocaleDateString())
     client.end();
   });
 });
