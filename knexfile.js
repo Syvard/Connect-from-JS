6cde2807be9bbb26f936f326bc32f606a1bd2b1c
Chris Northwood <chris.northwood@hotmail.com>
2017-11-22 02:29:23 +0000

    updated knexfile.

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+const settings = require("./settings");
+
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : settings.hostname,
+      user : settings.user,
+      password : settings.password,
+      database : settings.database
+
+    }
+  }
+}
